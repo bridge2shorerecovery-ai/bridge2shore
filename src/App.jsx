6e172c7ba293c21810af56152f20333c1c61faf8
Zack <zack.elsabeh@hotmail.com>
2026-05-08 13:04:39 -0700

    remove pre-launch banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import DevBanner from './components/DevBanner'
 import Home from './pages/Home'
 import Services from './pages/Services'
 import Partnerships from './pages/Partnerships'
@@ -23,8 +22,7 @@ function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <ScrollToTop />
-      <DevBanner />
-      <Navbar />
+<Navbar />
       <main className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
